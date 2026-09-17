40b9bd244587a9a133d8840556ad64d6e53a939d
GeovaniPassos <geovanipassos95@outlook.com>
2026-09-17 14:16:11 -0300

    Implementado a configuração de environments

--- src/app/categories/service/category.service.ts
+++ src/app/categories/service/category.service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from "@angular/core";
 import { Category } from "../model/category.model";
 import { Observable } from "rxjs";
 import { HttpClient } from "@angular/common/http";
+import { environment } from "../../../environments/environment.development";
 
 @Injectable({
     providedIn: 'root'
@@ -9,7 +10,7 @@ import { HttpClient } from "@angular/common/http";
 export class CategoryService {
     private readonly http = inject(HttpClient);
 
-    private readonly apiUrl = 'http://localhost:3000/categories';
+    private readonly apiUrl = `${environment.apiUrl}/categories`;
 
     findAll(): Observable<Category[]> {
         return this.http.get<Category[]>(this.apiUrl);
