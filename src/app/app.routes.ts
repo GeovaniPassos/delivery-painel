476939578ace20d5acf6ba1408fd693257e88264
GeovaniPassos <geovanipassos95@outlook.com>
2026-09-18 14:38:03 -0300

    Implementando o layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,18 @@
 import { Routes } from '@angular/router';
 import { Layout } from './layout/layout/layout';
+import { Categories } from './categories/categories';
+import { Orders } from './orders/orders';
+import path from 'path/win32';
 
 export const routes: Routes = [
     
     {
-        path: 'Layout',
+        path: 'Login',
         redirectTo: '',
         pathMatch: 'full'
     },
 
-    // caso a url não existir, ir para uma página de erro, ou para a home
+    // // caso a url não existir, ir para uma página de erro, ou para a home
     // { 
     //     path: '**', component: PageNotFoundComponent 
     // },
@@ -19,7 +22,11 @@ export const routes: Routes = [
         component: Layout,
 
         children: [
-            // { path: 'home', component: Home },
+            {   path: '',
+                redirectTo: 'pedidos',
+                pathMatch: 'full' },
+            { path: 'pedidos', component: Orders },
+            { path: 'categoria', component: Categories },
             // { path: 'produtos', component: ProductsGrid },
             // { path: 'carrinho', component: Cart },
             // { path: 'login', component: Login },
--- src/app/layout/layout/layout.ts
+++ src/app/layout/layout/layout.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Sidebar } from '../../sidebar/sidebar';
-import { Header } from '../../header/header';
+import { Sidebar } from '../sidebar/sidebar';
+import { Header } from '../header/header';
 
 @Component({
   imports: [RouterOutlet, Header, Sidebar],
